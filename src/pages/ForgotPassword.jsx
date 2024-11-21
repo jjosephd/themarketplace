0241418fdf6e38f267485c59192b6a3bbba22b4b
jjosephd <jjosephd804@gmail.com>
2024-11-21 15:50:36 -0500

    Forgot password functionality added

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -12,9 +12,74 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 import React from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
+import { toast } from 'react-toastify';
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
+import visibilityIcon from '../assets/svg/visibilityIcon.svg';
+
+import { ReactComponent as GoogleIcon } from '../assets/svg/googleIcon.svg';
 
 const ForgotPassword = () => {
-  return <div>Forgot Password</div>;
+  const [email, setEmail] = useState('');
+
+  const onChange = (e) => {
+    setEmail(e.target.value);
+  };
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      await sendPasswordResetEmail(auth, email);
+      toast.success('Email was sent', {
+        position: 'top-center',
+        autoClose: 2500,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        progress: undefined,
+      });
+    } catch (error) {
+      toast.error('Could not send reset email', {
+        position: 'top-center',
+        autoClose: 2500,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        progress: undefined,
+      });
+    }
+  };
+  return (
+    <div className="pageContainer">
+      <header>
+        <p className="pageHeader">Forgot Password</p>
+      </header>
+      <main>
+        <form onSubmit={onSubmit}>
+          <input
+            type="email"
+            className="emailInput"
+            name=""
+            id="email"
+            value={email}
+            onChange={onChange}
+            placeholder="Email"
+          />
+          <Link className="forgotPasswordLink" to="signin">
+            Sign In
+          </Link>
+          <div className="signInBar">
+            <div className="signInText">Send Reset Link</div>
+            <button className="signInButton">
+              <ArrowRightIcon fill="#ffffff" width="34px" height="34px" />
+            </button>
+          </div>
+        </form>
+      </main>
+    </div>
+  );
 };
 
 export default ForgotPassword;
